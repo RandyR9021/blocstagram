c89ac1c028edb32051884f19de273e4a129895f5
Randyr902 <rdj7890@gmail.com>
2016-01-03 08:51:48 -0800

    added comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,28 @@
 class CommentsController < ApplicationController
-  def show
-  end
-
-  def new
-  end
 
   def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.create(params[:comment])
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render 'new'
+    end
   end
 
   def destroy
-  end 
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    if @comment.destroy
+      flash[:notice] = "Comment was deleted successfully."
+    else
+      flash[:error] = "Comment couldnt't be deleted. Please try again."
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
+  has_many :comments
   has_attached_file :image, styles: { medium: "300x300>"}, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
